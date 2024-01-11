ff7fe56ab3aff379e356d34e341f937576797574
Ikemdinachi Uzochukwu <uzochukwuikemdinachi@gmail.com>
2024-01-11 20:19:28 +0100

    created projection for purchasehistory

--- src/main/java/com/cavsteek/bookseller/repository/PurchaseRepository.java
+++ src/main/java/com/cavsteek/bookseller/repository/PurchaseRepository.java
@@ -1,9 +1,18 @@
 package com.cavsteek.bookseller.repository;
 
 import com.cavsteek.bookseller.model.PurchaseHistory;
+import com.cavsteek.bookseller.repository.projection.PurchaseItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface PurchaseRepository extends JpaRepository<PurchaseHistory, Long>
 {
-
+    @Query("select" +
+            "b.title as title, ph.price as price, ph.purchaseTime as purchaseTime" +
+            "from PurchaseHistory ph left join Book b on b.id = ph.bookId" +
+            "where ph.userId = :userId")
+    List<PurchaseItem> findAllPurchasesOfUser(@Param("userId") Long userId);
 }
