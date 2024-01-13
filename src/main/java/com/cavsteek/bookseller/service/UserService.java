e11ed20c5b9026b831bd628fef64cb602b312c46
Ikemdinachi Uzochukwu <uzochukwuikemdinachi@gmail.com>
2024-01-13 11:15:02 +0100

    Setup userservice class

--- src/main/java/com/cavsteek/bookseller/service/UserService.java
+++ src/main/java/com/cavsteek/bookseller/service/UserService.java
@@ -1,6 +1,17 @@
 package com.cavsteek.bookseller.service;
 
+import com.cavsteek.bookseller.model.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 public interface UserService {
+    UserDetailsService userDetailsService();
+
+    User saveUser(User user);
+
+    Optional<User> findByUsername(String username);
+
+    void makeAdmin(String username);
 }
--- src/main/java/com/cavsteek/bookseller/service/impl/UserServiceImpl.java
+++ src/main/java/com/cavsteek/bookseller/service/impl/UserServiceImpl.java
@@ -1,12 +1,56 @@
 package com.cavsteek.bookseller.service.impl;
 
+import com.cavsteek.bookseller.model.User;
+import com.cavsteek.bookseller.model.Role;
 import com.cavsteek.bookseller.repository.UserRepository;
 import com.cavsteek.bookseller.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public UserDetailsService userDetailsService(){
+        return new UserDetailsService() {
+            @Override
+            public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+                return userRepository.findByUsername(username)
+                        .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+            }
+        };
+    }
+
+    @Override
+    public User saveUser(User user)
+    {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user.setRole(Role.USER);
+        user.setCreateTime(LocalDateTime.now());
+
+        return userRepository.save(user);
+    }
+
+    @Override
+    public Optional<User> findByUsername(String username){
+        return userRepository.findByUsername(username);
+    }
+
+    @Transactional
+    @Override
+    public void makeAdmin(String username)
+    {
+        userRepository.updateUserRole(username, Role.ADMIN);
+    }
 }
