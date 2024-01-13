15745d50ba73a7d0cdf451d92db6ed65adf976cd
Ikemdinachi Uzochukwu <uzochukwuikemdinachi@gmail.com>
2024-01-13 11:28:09 +0100

    Setup purchasehistoryservice class

--- src/main/java/com/cavsteek/bookseller/service/PurchaseHistoryService.java
+++ src/main/java/com/cavsteek/bookseller/service/PurchaseHistoryService.java
@@ -1,4 +1,12 @@
 package com.cavsteek.bookseller.service;
 
+import com.cavsteek.bookseller.model.PurchaseHistory;
+import com.cavsteek.bookseller.repository.projection.PurchaseItem;
+
+import java.util.List;
+
 public interface PurchaseHistoryService {
+    PurchaseHistory savePurchaseHistory(PurchaseHistory purchaseHistory);
+
+    List<PurchaseItem> findPurchasedItems(Long userId);
 }
--- src/main/java/com/cavsteek/bookseller/service/impl/PurchaseHistoryServiceImpl.java
+++ src/main/java/com/cavsteek/bookseller/service/impl/PurchaseHistoryServiceImpl.java
@@ -1,12 +1,28 @@
 package com.cavsteek.bookseller.service.impl;
 
+import com.cavsteek.bookseller.model.PurchaseHistory;
 import com.cavsteek.bookseller.repository.PurchaseHistoryRepository;
+import com.cavsteek.bookseller.repository.projection.PurchaseItem;
 import com.cavsteek.bookseller.service.PurchaseHistoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class PurchaseHistoryServiceImpl implements PurchaseHistoryService {
     private final PurchaseHistoryRepository purchaseHistoryRepository;
+
+    @Override
+    public PurchaseHistory savePurchaseHistory(PurchaseHistory purchaseHistory) {
+        purchaseHistory.setPurchaseTime(LocalDateTime.now());
+        return purchaseHistoryRepository.save(purchaseHistory);
+    }
+
+    @Override
+    public List<PurchaseItem> findPurchasedItems(Long userId){
+        return purchaseHistoryRepository.findAllPurchasesOfUser(userId);
+    }
 }
