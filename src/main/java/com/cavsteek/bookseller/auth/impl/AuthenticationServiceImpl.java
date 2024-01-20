478fac0db936842b2248dc582a7a12e15d941c23
Ikemdinachi Uzochukwu <uzochukwuikemdinachi@gmail.com>
2024-01-20 18:44:02 +0100

    setup security

--- src/main/java/com/cavsteek/bookseller/auth/impl/AuthenticationServiceImpl.java
+++ src/main/java/com/cavsteek/bookseller/auth/impl/AuthenticationServiceImpl.java
@@ -1,12 +1,21 @@
 package com.cavsteek.bookseller.auth.impl;
 
 import com.cavsteek.bookseller.auth.AuthenticationService;
+import com.cavsteek.bookseller.dto.JwtAuthenticationResponse;
+import com.cavsteek.bookseller.dto.RefreshTokenRequest;
+import com.cavsteek.bookseller.dto.SignInRequest;
+import com.cavsteek.bookseller.dto.SignUpRequest;
 import com.cavsteek.bookseller.jwt.JWTService;
+import com.cavsteek.bookseller.model.Role;
+import com.cavsteek.bookseller.model.User;
 import com.cavsteek.bookseller.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
 
 @Service
 @RequiredArgsConstructor
@@ -16,5 +25,49 @@ public class AuthenticationServiceImpl implements AuthenticationService {
     private final JWTService jwtService;
     private final PasswordEncoder passwordEncoder;
 
+    public User signUp(SignUpRequest signUpRequest, Role role){
+        User user = new User();
+        user.setUsername(signUpRequest.getUsername());
+        user.setFirstName(signUpRequest.getFirstName());
+        user.setLastName(signUpRequest.getLastName());
+        user.setRole(Role.USER);
+        user.setPassword(
+                passwordEncoder.encode(signUpRequest.getPassword())
+        );
 
+        return userRepository.save(user);
+    }
+
+    public JwtAuthenticationResponse signIn(SignInRequest signInRequest){
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
+                signInRequest.getUsername(),
+                signInRequest.getPassword()));
+        var user = userRepository.findByUsername(signInRequest.getUsername())
+                .orElseThrow(
+                        () -> new IllegalArgumentException("Invalid Username or Password")
+                );
+        var jwt = jwtService.generateToken(user);
+        var refreshToken = jwtService.generateRefreshToken(new HashMap<>(), user);
+
+        JwtAuthenticationResponse jwtAuthenticationResponse = new JwtAuthenticationResponse();
+
+        jwtAuthenticationResponse.setToken(jwt);
+        jwtAuthenticationResponse.setRefreshToken(refreshToken);
+        return jwtAuthenticationResponse;
+    }
+
+    public JwtAuthenticationResponse refreshToken(RefreshTokenRequest refreshTokenRequest){
+        String userName = jwtService.extractUsername(refreshTokenRequest.getToken());
+        User user = userRepository.findByUsername(userName).orElseThrow();
+        if (jwtService.isTokenValid(refreshTokenRequest.getToken(), user)){
+            var jwt = jwtService.generateToken(user);
+
+            JwtAuthenticationResponse jwtAuthenticationResponse = new JwtAuthenticationResponse();
+
+            jwtAuthenticationResponse.setToken(jwt);
+            jwtAuthenticationResponse.setRefreshToken(refreshTokenRequest.getToken());
+            return jwtAuthenticationResponse;
+        }
+        return null;
+    }
 }
