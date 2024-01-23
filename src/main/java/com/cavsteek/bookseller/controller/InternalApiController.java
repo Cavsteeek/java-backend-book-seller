d6c68527184e79cfe7177571052c7cdef9dad687
Ikemdinachi Uzochukwu <uzochukwuikemdinachi@gmail.com>
2024-01-23 13:08:47 +0100

    created internal api controller

---
+++ src/main/java/com/cavsteek/bookseller/controller/InternalApiController.java
@@ -0,0 +1,21 @@
+package com.cavsteek.bookseller.controller;
+
+import com.cavsteek.bookseller.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("ap/v1/internal")
+public class InternalApiController {
+    private UserService userService;
+
+    @PutMapping("make-admin/{username}")
+    public ResponseEntity<?> makeAdmin(@PathVariable String username){
+        userService.makeAdmin(username);
+
+        return ResponseEntity.ok("Created");
+    }
+}
