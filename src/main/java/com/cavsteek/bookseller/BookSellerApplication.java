69efc97609aaf043f51f6da27432d2115ec3ab93
Ikemdinachi Uzochukwu <uzochukwuikemdinachi@gmail.com>
2024-01-25 09:16:28 +0100

    implemented creation of admin on app startup

--- src/main/java/com/cavsteek/bookseller/BookSellerApplication.java
+++ src/main/java/com/cavsteek/bookseller/BookSellerApplication.java
@@ -1,13 +1,39 @@
 package com.cavsteek.bookseller;
 
+import com.cavsteek.bookseller.model.Role;
+import com.cavsteek.bookseller.model.User;
+import com.cavsteek.bookseller.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.time.LocalDateTime;
 
 @SpringBootApplication
-public class BookSellerApplication {
+@RequiredArgsConstructor
+public class BookSellerApplication implements CommandLineRunner {
+
+	private final UserRepository userRepository;
 
 	public static void main(String[] args) {
-		SpringApplication.run(BookSellerApplication.class, args);
+		try{
+			SpringApplication.run(BookSellerApplication.class, args);
+		}catch (Exception e){
+			e.printStackTrace();
+		}
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		User admin = new User();
+		admin.setUsername("Admin");
+		admin.setFirstName("Hober");
+		admin.setFirstName("Malo");
+		admin.setRole(Role.ADMIN);
+		admin.setCreateTime(LocalDateTime.now());
+		admin.setPassword(new BCryptPasswordEncoder().encode("iadmin"));
+		userRepository.save(admin);
+	}
 }
