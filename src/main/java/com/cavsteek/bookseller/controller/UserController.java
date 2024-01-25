c8b205a897de459176c1ff062fb647f96322f56b
Ikemdinachi Uzochukwu <uzochukwuikemdinachi@gmail.com>
2024-01-25 09:28:17 +0100

    created dummy user account for tests

---
+++ src/main/java/com/cavsteek/bookseller/controller/UserController.java
@@ -0,0 +1,24 @@
+package com.cavsteek.bookseller.controller;
+
+
+import com.cavsteek.bookseller.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/user")
+@RequiredArgsConstructor
+public class UserController {
+    // private final UserRepository userRepository;
+    @GetMapping
+    public ResponseEntity<String> sayHello(){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String username = authentication.getName();
+        return ResponseEntity.ok("Welcome User "+ username );
+    }
+}
