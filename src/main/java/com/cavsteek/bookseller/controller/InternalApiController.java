751c179ed481f73a81655c2bf7b7bec023d698d6
Ikemdinachi Uzochukwu <uzochukwuikemdinachi@gmail.com>
2024-01-27 20:25:25 +0100

    fixed errors on internal api controller

--- src/main/java/com/cavsteek/bookseller/controller/InternalApiController.java
+++ src/main/java/com/cavsteek/bookseller/controller/InternalApiController.java
@@ -1,6 +1,8 @@
 package com.cavsteek.bookseller.controller;
 
 import com.cavsteek.bookseller.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/v1/internal")
+@RequiredArgsConstructor
 public class InternalApiController {
     private UserService userService;
 
@@ -16,6 +19,6 @@ public class InternalApiController {
     public ResponseEntity<?> makeAdmin(@PathVariable String username){
         userService.makeAdmin(username);
 
-        return ResponseEntity.ok("Created");
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
