1816ffa8e1172d869d5655aa29ed8daac55e27cf
Ikemdinachi Uzochukwu <uzochukwuikemdinachi@gmail.com>
2024-01-29 09:59:14 +0100

    refactored security config class

--- src/main/java/com/cavsteek/bookseller/config/SecurityConfig.java
+++ src/main/java/com/cavsteek/bookseller/config/SecurityConfig.java
@@ -38,7 +38,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(request -> request
                         .requestMatchers("/api/v1/auth/**").permitAll()
                         .requestMatchers(HttpMethod.GET,"/api/v1/book").permitAll()
-                        .requestMatchers("/api/v1/book/**").hasRole(Role.ADMIN.name())
+                        .requestMatchers("/api/v1/book/**").hasAnyAuthority(Role.ADMIN.name())
                         .anyRequest()
                         .authenticated()
                 )
