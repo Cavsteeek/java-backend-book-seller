387529eb4b35196165b6894ebe4c7546454cc59f
Ikemdinachi Uzochukwu <uzochukwuikemdinachi@gmail.com>
2024-04-05 22:19:58 +0100

    feat:configured cloudinary class

---
+++ src/main/java/com/cavsteek/bookseller/config/CloudinaryConfiguration.java
@@ -0,0 +1,27 @@
+package com.cavsteek.bookseller.config;
+
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CloudinaryConfiguration {
+    @Value("${cloudinary.cloud_name}")
+    private String cloudName;
+
+    @Value("${cloudinary.api_key}")
+    private String apiKey;
+
+    @Value("${cloudinary.api_secret}")
+    private String apiSecret;
+
+    @Bean
+    public Cloudinary cloudinary() {
+        return new Cloudinary(ObjectUtils.asMap(
+                "dijp7xnb3", cloudName,
+                "651778797976316", apiKey,
+                "LiOpX8lFaeHkT107CroRlyoHS30", apiSecret));
+    }
+}
