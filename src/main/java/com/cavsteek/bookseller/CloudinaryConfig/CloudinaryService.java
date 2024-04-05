59287e9bcc51898fdf69deeee5f2c6ac30fd6c06
Ikemdinachi Uzochukwu <uzochukwuikemdinachi@gmail.com>
2024-04-05 22:53:31 +0100

    setup cloudinary service class and it's impl

--- src/main/java/com/cavsteek/bookseller/CloudinaryConfig/CloudinaryService.java
+++ src/main/java/com/cavsteek/bookseller/CloudinaryConfig/CloudinaryService.java
@@ -1,4 +1,7 @@
 package com.cavsteek.bookseller.CloudinaryConfig;
 
+import org.springframework.web.multipart.MultipartFile;
+
 public interface CloudinaryService {
+    String uploadFile(MultipartFile file, String folderName);
 }
