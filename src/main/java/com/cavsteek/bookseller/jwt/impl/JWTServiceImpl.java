6b012fe07f3f65354c95598352775f2970e47391
Ikemdinachi Uzochukwu <uzochukwuikemdinachi@gmail.com>
2024-04-29 09:00:35 +0100

    fix: changed the jwt exp time

--- src/main/java/com/cavsteek/bookseller/jwt/impl/JWTServiceImpl.java
+++ src/main/java/com/cavsteek/bookseller/jwt/impl/JWTServiceImpl.java
@@ -24,7 +24,7 @@ public class JWTServiceImpl implements JWTService {
                 .builder()
                 .setSubject(userDetails.getUsername())
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis()+ 1000 * 60 * 24))
+                .setExpiration(new Date(System.currentTimeMillis()+ 1000 * 60 * 60 * 24))
                 .signWith(getSignInKey(), SignatureAlgorithm.HS256)
                 .compact();
     }
