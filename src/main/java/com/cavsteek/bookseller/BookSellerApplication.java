35e9aece92045501ec624bb0d83dad3160035de0
Cavsteeek <uzochukwuikemdinachi@gmail.com>
2024-09-04 00:10:42 +0100

    fix: more changes

--- src/main/java/com/cavsteek/bookseller/BookSellerApplication.java
+++ src/main/java/com/cavsteek/bookseller/BookSellerApplication.java
@@ -15,35 +15,32 @@ import java.time.LocalDateTime;
 @RequiredArgsConstructor
 public class BookSellerApplication implements CommandLineRunner {
 
-	private final UserRepository userRepository;
+    private final UserRepository userRepository;
 
+    public static void main(String[] args) {
+        try {
+            SpringApplication.run(BookSellerApplication.class, args);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-
-	public static void main(String[] args) {
-		try{
-			SpringApplication.run(BookSellerApplication.class, args);
-		}catch (Exception e){
-			e.printStackTrace();
-		}
-	}
-
-
-	@Override
-	public void run(String... args) throws Exception {
-		User adminAccount = userRepository.findByRole(Role.ADMIN);
-			if (null == adminAccount) {
-				User admin = new User();
-				admin.setUsername("Admin");
-				admin.setFirstName("Hober");
-				admin.setLastName("Malo");
-				admin.setEmail("admin@gmail.com");
-				admin.setRole(Role.ADMIN);
-				admin.setCreateTime(LocalDateTime.now());
-				admin.setPassword(new BCryptPasswordEncoder().encode("iadmin"));
-				userRepository.save(admin);
-				System.out.println("New admin created successfully!");
-			} else {
-				System.out.println("Admin with username 'Admin' already exists. Skipping creation.");
-			}
-	}
+    @Override
+    public void run(String... args) throws Exception {
+        User adminAccount = userRepository.findByRole(Role.ADMIN);
+        if (null == adminAccount) {
+            User admin = new User();
+            admin.setUsername("Admin");
+            admin.setFirstName("Hober");
+            admin.setLastName("Malo");
+            admin.setEmail("admin@gmail.com");
+            admin.setRole(Role.ADMIN);
+            admin.setCreateTime(LocalDateTime.now());
+            admin.setPassword(new BCryptPasswordEncoder().encode("iadmin"));
+            userRepository.save(admin);
+            System.out.println("New admin created successfully!");
+        } else {
+            System.out.println("Admin with username 'Admin' already exists. Skipping creation.");
+        }
+    }
 }
